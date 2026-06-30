24e3495faaef643ea2a384efd90c921ea52eb7ed
sheyito27 <46390546+sheyito27@users.noreply.github.com>
2026-07-01 01:04:08 +0200

    Implementar los middlewares en routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,11 @@
 import * as goalController from './controllers/goalController.js'
 import * as taskController from './controllers/taskController.js'
 import { Router } from 'express';
-// Importa tus esquemas (ejemplos)
+
 import { GoalSchema, UpdateGoalSchema } from './schemas/goalSchema.js';
-import { TaskSchema, UpdateTaskSchema } from './schemas/taskSchema.js';
+import { taskParamsSchema, TaskSchema, UpdateTaskSchema } from './schemas/taskSchema.js';
 import { GoalParamsSchema } from './schemas/goalSchema.js';
 
-// Importa tus middlewares
 import { validate } from './middlewares/validate.js';
 import { asyncHandler } from './middlewares/catchAsync.js';
 
@@ -23,7 +22,7 @@ goalRouter.delete("/:id", validate({ params: GoalParamsSchema }), asyncHandler(g
 export const taskRouter = Router();
 
 taskRouter.get("/", asyncHandler(taskController.getAllTasks));
-taskRouter.get("/:id", validate({ params: IdParamSchema }), asyncHandler(taskController.getTaskById));
-taskRouter.post("/", validate({ body: CreateTaskSchema }), asyncHandler(taskController.createTask));
-taskRouter.patch("/:id", validate({ params: IdParamSchema, body: UpdateTaskSchema }), asyncHandler(taskController.updateTask));
-taskRouter.delete("/:id", validate({ params: IdParamSchema }), asyncHandler(taskController.deleteTask));
+taskRouter.get("/:id", validate({ params: taskParamsSchema }), asyncHandler(taskController.getTaskById));
+taskRouter.post("/", validate({ body: TaskSchema }), asyncHandler(taskController.createTask));
+taskRouter.patch("/:id", validate({ params: taskParamsSchema, body: UpdateTaskSchema }), asyncHandler(taskController.updateTask));
+taskRouter.delete("/:id", validate({ params: taskParamsSchema }), asyncHandler(taskController.deleteTask));
